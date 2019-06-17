5bd9005070c60f84a4cde533b9889cf358e40eac
loushang <loushang@vipkid.com.cn>
2019-06-17 13:26:39 +0800

    chapter12 done

--- chapter12/src/main/java/nia/chapter12/ChatServerInitializer.java
+++ chapter12/src/main/java/nia/chapter12/ChatServerInitializer.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
@@ -26,11 +28,24 @@ public class ChatServerInitializer extends ChannelInitializer<Channel> {
     //将所有需要的 ChannelHandler 添加到 ChannelPipeline 中
     protected void initChannel(Channel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
+
+        //将字节解码为 HttpRequest、 HttpContent 和 LastHttpContent 。并将 HttpRequest 、 HttpContent 和 LastHttpContent 编码为字节
         pipeline.addLast(new HttpServerCodec());
+
+        //写入一个文件的内容
         pipeline.addLast(new ChunkedWriteHandler());
+
+        //将一个 HttpMessage 和跟随它的多个 HttpContent 聚合 为单个 FullHttpRequest 或者 FullHttpResponse （取决于它是被用来处理请求还是响应）。
+        // 安装了这个之后，ChannelPipeline 中的下一个 ChannelHandler 将只会收到完整的 HTTP 请求或响应
         pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+
+        //处理 FullHttpRequest (哪些不发送到 /ws URI的请求)
         pipeline.addLast(new HttpRequestHandler("/ws"));
+
+        //按照 WebSocket 规范的要求，处理 WebSocket 升级握手、PingWebSocketFrame 、 PongWebSocketFrame 和 CloseWebSocketFrame
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+
+        //处理 TextWebSocketFrame 和握手完成 事件
         pipeline.addLast(new TextWebSocketFrameHandler(group));
     }
 }
--- chapter12/src/main/java/nia/chapter12/TextWebSocketFrameHandler.java
+++ chapter12/src/main/java/nia/chapter12/TextWebSocketFrameHandler.java
@@ -42,6 +42,11 @@ public class TextWebSocketFrameHandler
     public void channelRead0(ChannelHandlerContext ctx,
         TextWebSocketFrame msg) throws Exception {
         //(3) 增加消息的引用计数，并将它写到 ChannelGroup 中所有已经连接的客户端
+        /**
+         * 和之前一样，对于 retain() 方法的调用是必需的，因为当 channelRead0() 方法返回时，
+         * TextWebSocketFrame 的引用计数将会被减少。由于所有的操作都是异步的，因此， writeAndFlush() 方法
+         * 可能会在 channelRead0() 方法返回之后完成，而且它绝对不能访问一个已经失效的引用。
+         */
         group.writeAndFlush(msg.retain());
     }
 }
